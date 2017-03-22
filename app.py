634cad52c67c9c50f41a90e6fe04961276ed456e
Martti Löyttyniemi <Martti Lyttyniemi>
2017-03-22 23:33:46 +0200

    postgresia toimiin

--- app.py
+++ app.py
@@ -7,7 +7,20 @@ app = Flask(__name__)
 
 app.secret_key = 'why would I tell you my secret key, you asshole?'
 
+import os
+import psycopg2
+import urlparse
 
+urlparse.uses_netloc.append("postgres")
+url = urlparse.urlparse(os.environ["DATABASE_URL"])
+
+conn = psycopg2.connect(
+    database=url.path[1:],
+    user=url.username,
+    password=url.password,
+    host=url.hostname,
+    port=url.port
+)
 
 @app.route("/")
 def main():
