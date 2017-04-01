8075a9b8dac6d8f5e03cf8df28440a12b10861aa
Martti <mara92@gmail.com>
2017-04-01 12:49:43 +0300

    lisääkorjausta

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, json, request, redirect, session
 from flask_sqlalchemy import SQLAlchemy
-from flask_heroku import Heroku
 from werkzeug import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
