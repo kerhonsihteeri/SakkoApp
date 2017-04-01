d844e711021553a1f91dd8c555fcaba5ce923652
Martti <mara92@gmail.com>
2017-04-01 13:03:12 +0300

    lisääkorjausta4

--- models.py
+++ models.py
@@ -7,16 +7,16 @@ from datetime import datetime
 class User(db.Model):
 	__tablename__ = "tbl_user"
 	user_id = db.Column(db.Integer, primary_key=True)
-	user_name = db.Column(db.Varchar(45))
-	user_username = db.Column(db.Varchar(45), unique=True)
-	user_email = db.Column(db.Varchar(120), unique=True)
-	user_password = db.Column(db.Varchar(256))
+	user_name = db.Column(db.String(45))
+	user_username = db.Column(db.String(45), unique=True)
+	user_email = db.Column(db.String(120), unique=True)
+	user_password = db.Column(db.String(256))
 	
 #cearing sakkodb model	
 class Sakko (db.Model):
 	__tablename__ = "tbl_sakko"
 	sakko_id = db.Column(db.Integer, primary_key=True)
-	sakko_title = db.Column(db.Varchar(45))
-	sakko_description = db.Column(db.Varchar(5000))
+	sakko_title = db.Column(db.String(45))
+	sakko_description = db.Column(db.String(5000))
 	sakko_user_id = db.Column(db.Integer)
 	sakko_date = db.Column(db.datetime)
